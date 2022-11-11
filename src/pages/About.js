76e693151516df3eab54b430ae69389a8cb31da6
Sardor <abdullaevss94@mail.ru>
2022-11-11 12:34:04 +0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+        sapiente numquam consequuntur blanditiis rerum excepturi dolores vel
+        alias molestiae non, suscipit distinctio recusandae perferendis sunt
+        earum aliquam quaerat vero nostrum provident exercitationem ut
+        necessitatibus laborum maiores? Dolor minus vero sapiente.
+      </p>
+    </section>
+  );
+};
+
+export default About;
